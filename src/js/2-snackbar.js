4e6e9f1ec2b3e5fac23d1f100ec0f9c10a4c362a
olyaYur <141238454+olyaYur@users.noreply.github.com>
2024-01-12 23:22:41 +0200

    update.js

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,54 @@
+"use strict";
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+let inputValue = form.elements.delay.value;
+let inputState = form.elements.state.value;
+
+
+
+
+
+function createPromise (inputState, inputValue ){
+    return new Promise((resolve, reject)=> {
+    setTimeout(() => {
+      if(inputState === "fulfilled"){
+        resolve(inputValue);
+      } else {
+        reject(inputValue);
+      }   
+    }, inputValue);
+});
+};
+
+function handleCreateNotification(event){
+    event.preventDefault();
+    let inputState= form.elements.state.value;
+    let inputValue = form.elements.delay.value;
+
+    createPromise(inputState, inputValue) 
+    .then(inputValue =>{
+        iziToast.success({
+            position: 'topRight',
+            title: 'OK',
+            message: `✅ Fulfilled promise in ${inputValue}ms`,
+          });   
+    })
+    .catch(error =>{
+        iziToast.error({
+            position: 'topRight',
+            title: 'Error',
+            message: `❌ Rejected promise in ${error}ms`,
+          });  
+    })
+    .finally(() => {
+        form.reset();
+      });
+   
+      inputValue = "";  
+};
+
+form.addEventListener("submit", handleCreateNotification);
+
